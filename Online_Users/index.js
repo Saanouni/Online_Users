9b1a9597d1d73341d79dd51187cac04fc5af147d
Saanouni <88947833+Saanouni@users.noreply.github.com>
2021-10-25 13:25:46 +0100

    Update index.js

--- Online_Users/index.js
+++ Online_Users/index.js
@@ -1,12 +1,4 @@
-/* 
-    Challenge
-    
-    Using the body tag as a Flexbox container
-        re-create the provided layout
-    
-    Hint: The Header and Footer are each 10% 
-        of the container's height
-*/
+
 
 async function getUsers() {
     let response = await fetch("users.json")
